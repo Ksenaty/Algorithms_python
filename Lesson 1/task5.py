97663a83a4bed57556d0c086d134a243fbd67f7a
Yaroslav <73306309+Ksenaty@users.noreply.github.com>
2021-02-21 13:08:58 +0300

    Revert "added lesson 1 task 5"
    This reverts commit 493ac8eb

--- Lesson 1/task5.py
+++ Lesson 1/task5.py
@@ -1,15 +1,10 @@
 # Пользователь вводит две буквы. Определить, на каких местах алфавита они стоят, и сколько между ними находится букв.
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-sym1 = str(input('Введите первую букву <<< '))
-sym2 = str(input('Введите вторую <<< '))
+sym1 = str(input('Введите первую букву <<<'))
+sym2 = str(input('Введите вторую <<<'))
 sym1_index = alphabet.find(sym1)
 sym2_index = alphabet.find(sym2)
-if sym2_index > sym1_index:
-    print(f'Индекс первой буквы {sym1_index}\n'
-          f'Индекс второй буквы {sym2_index}\n'
-          f'Букв между ними {len(alphabet[sym1_index + 1:sym2_index])}\n')
-else:
-    print(f'Индекс первой буквы {sym1_index}\n'
-          f'Индекс второй буквы {sym2_index}\n'
-          f'Букв между ними {sym1_index - (sym2_index + 1)}\n')
+print(f'Индекс первой буквы {sym1_index}\n'
+      f'Индекс второй буквы {sym2_index}\n'
+      f'Букв между ними {len(alphabet[sym1_index + 1:sym2_index])}\n')
